0aa0e32f55773ea918945382029ac6bdebad0b7a
Mert Metin <mertmetin@outlook.com>
2020-10-21 10:28:09 -0400

    fix.

--- dev/www/api/index.php
+++ dev/www/api/index.php
@@ -10,8 +10,8 @@
  */
 
 // TODO: Get vendor path automatically.
-if (file_exists(__DIR__ . '../../../../vendor/autoload.php'))
-    require_once(__DIR__ . '../../../../vendor/autoload.php');
+if (file_exists('../../../vendor/autoload.php'))
+    require_once('../../../vendor/autoload.php');
 else
     require_once('../../../../../autoload.php');
 
