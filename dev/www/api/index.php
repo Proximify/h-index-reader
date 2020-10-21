641d28ef21089f1fd318318b7bee95a9109c43a2
Mert Metin <mertmetin@outlook.com>
2020-10-21 10:08:05 -0400

    Fix.

--- dev/www/api/index.php
+++ dev/www/api/index.php
@@ -9,7 +9,11 @@
  * @version   1.0
  */
 
-require_once __DIR__ . '../../../../vendor/autoload.php';
+// TODO: Get vendor path automatically.
+if (file_exists(__DIR__ . '../../../../vendor/autoload.php'))
+    require_once(__DIR__ . '../../../../vendor/autoload.php');
+else
+    require_once('../../../../../autoload.php');
 
 use Proximify\HIndexReader;
 
